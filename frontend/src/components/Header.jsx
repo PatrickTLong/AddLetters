794fa813426f3c9e2224904f1437527c0e284a2b
Patrick Long <longpatrick3317@gmail.com>
2025-01-18 16:01:19 -0500

    dawg

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,24 +2,47 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Header() {
+    {/* SVGS */}
+    const trashsvg = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+  </svg>
     {/* STATES */}
     const [data, setdata] = useState({ summary: "" });
     const [getdata, setgetdata] = useState([]);
     const [checked, setchecked] = useState([]);
+    const [empty, setempty] = useState(true)
 
     {/* FUNCTIONS */}
+    {/* DELETE STATE & DATA */}
+    function Delete(index, object) {
+        
+        {/* POST DELETE DATABASE DATA */}
+        const deletedata = {id : index, summary : object.summary}
+        axios.post("http://127.0.0.1:5000/deletechecklistdatastate", deletedata)
+        .then(() => 
+            setchecked(item => item.filter(prev => String(prev.location) !== index)),
+            setgetdata(item => item.filter(prev => prev.summary !== object.summary))
+        )
+    }
     {/* POST & GET */}
     function Submit(e) {
-        console.log(checked);
-        e.preventDefault();
-        axios.post("http://127.0.0.1:5000/checklistdata", data)
-            .then(() => {
-                axios.get("http://127.0.0.1:5000/returnchecklistdata")
-                    .then(response => {
-                        setgetdata(response.data);
-                    });
-            });
-        setdata(prev => ({ ...prev, summary: "" }));
+        if (data !== "") {
+
+            setempty(true)
+            e.preventDefault();
+            axios.post("http://127.0.0.1:5000/checklistdata", data)
+                .then(() => {
+                    axios.get("http://127.0.0.1:5000/returnchecklistdata")
+                        .then(response => {
+                            setgetdata(response.data);
+                        });
+                });
+                setdata(prev => ({...prev, summary : ""}))
+            }
+        else {
+            setempty(true)
+        }
+        
     }
 
     {/* GET checklist data */}
@@ -44,6 +67,12 @@ export default function Header() {
     }, []);
 
     function ChangeData(e) {
+        if (e.target.value !== "") {
+            setempty(false)
+        }
+        if (e.target.value === "") {
+            setempty(true)
+        }
         setdata(prev => ({ ...prev, summary: e.target.value }));
     }
 
@@ -86,12 +115,17 @@ export default function Header() {
                     className="w-72 border-2 font-medium placeholder:font-medium p-2 rounded-2xl border-black"
                     type="text"
                 />
-                <button className="rounded-2xl outline p-3 transition-all hover:bg-emerald-400 text-white font-medium bg-emerald-300 " onClick={Submit} type="submit">
+                <button className={`rounded-2xl outline duration-500 p-3 transition-all ${empty ? "bg-slate-400 pointer-events-none" : "bg-emerald-300"} text-white font-medium bg-emerald-300`} onClick={Submit} type="submit">
                     Submit
                 </button>
                 {/* Mapped Items */}
                 {getdata.map((object, index) => (
                     <div key={index} className="flex gap-3 ml-96 self-start items-center">
+                        <button onClick={() => Delete(index, object)} className="">
+                            {trashsvg}
+                        </button>
+                        
+                        
                         <input
                             className="w-3 h-3"
                             onChange={() => SetState(index)}
@@ -110,6 +144,7 @@ export default function Header() {
                             }`} 
                         >
                             {object.summary}
+                            
                         </p>
                     </div>
                 ))}
